b7829f34c1ddd5be8fef73e13604ed94e84aed90
LauLau13 <lauragilf.13@gmail.com>
2023-02-23 19:22:29 +0100

    Update Reservations.cs

--- Models/Reservations.cs
+++ Models/Reservations.cs
@@ -3,8 +3,8 @@ namespace WebApi.Models;
 public class Reservations
 {
     public string? Id { get; set; }
-    public Users Users { get; set; }
-    public Restaurants Restaurants { get; set; }
+    public Users User { get; set; }
+    public Restaurants Restaurant { get; set; }
     public int Personas { get; set; }
     public List<Features> Servicios { get; set; }
     public string InstruccionesEspeciales { get; set; }
