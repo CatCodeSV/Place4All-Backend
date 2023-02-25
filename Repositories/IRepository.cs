ec8df64084f6aeba64bb0d9f35ebac64024aee3d
SANDARA\Sandra <a25660@svalero.com>
2023-02-25 16:32:41 +0100

    repository

---
+++ Repositories/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+/*namespace WebApi.Repositories
+{
+	public class IRepository<T> : IDisposable where T : class
+    {
+        //Listas todos
+        Task<IEnumerable<T>> GetAll();
+        //Buscar por ID
+        Task<T> GetByID(int id);
+        //Insertar
+        Task<T> Insert(T context); //context era entity
+        //Eliminar
+        Task<T> Delete(int id);
+        //Actualizar
+        Task Update(T context);
+
+    }
+
+}*/
---
+++ Repositories/Repository.cs
@@ -0,0 +1,91 @@
+﻿/*using MongoDB.Driver;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Contexts;
+using System.Threading.Tasks;
+
+namespace WebApi.Repositories
+{
+	/*public class Repository<T>
+	{
+		private IList<T> list;
+
+		public Repository()
+		{
+			list=new List<T>
+		}
+	}*/
+
+    //Nuevo de learn
+  /*  public class Repository<T> : IRepository<T> where T: class
+    {
+
+        private readonly IMongoCollection context;
+
+        public Repository(IMongoCollection context)
+        {
+            this.context = context;
+        }
+
+        //Coger todos los datos
+        public async Task<IEnumerable<T>> GetAll();
+        {
+            return await context.ToListAsync();
+        }
+        //Buscar por id
+        public async Task<T> GetByID(int id);
+        {
+           return await context.FindAsync();
+        }
+        //Insertar
+        public async Task<T> Insert(T context);
+        {
+            context.Add(context);
+            await Save();
+            return context;
+        }
+        //Borrar por id
+        public async Task<T> Delete(int id);
+        {
+            T context = await context.FindAsync(id);
+            context.Remove(context);
+            await Save();
+            return context;
+        }
+        //Actualizar
+        public async Task Update(T context);
+        {
+            context.Entry(context).State = context.Modified;
+            await Save();
+        }
+        //Guardar
+        public async Task Save()
+        {
+            await context.SaveChangesAsync();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}*/
+
+
+//Se tienen que cambiar los servicios por este repositorio, pero como??
