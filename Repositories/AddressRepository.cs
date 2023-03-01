a6862db560b13f4acd662ffa0b91bce8f09b934b
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-03-01 17:18:21 +0100

    interfaz address

---
+++ Repositories/AddressRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Immutable;
+using WebApi.Models;
+using WebApi.Services;
+
+namespace WebApi.Repositories
+{
+    public class AddressRepository : IAddressRepository
+    {
+        private AddressesService _addresses { get; set; }
+        AddressesService IAddressRepository._addresses { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        private Addresses direccion;
+
+        //public List<Addresses> Get() => _direcciones.Find(direccion => true).ToList();
+        public async Task<List<Addresses>> Get()
+        {
+            return await _addresses.GetAddressesAsync();
+        }
+    }
+}
---
+++ Repositories/IAddressRepository.cs
@@ -0,0 +1,16 @@
+﻿using WebApi.Models;
+using WebApi.Services;
+
+namespace WebApi.Repositories
+{
+    public interface IAddressRepository
+    {
+        public AddressesService _addresses { get; set; }
+
+        public Task<List<Addresses>> GetAddressesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
