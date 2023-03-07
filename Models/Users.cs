91deacae68a8ab7a51b37e140915064311af7c77
Laura Gil <a25589svalero.com>
2023-03-07 18:20:54 +0100

    Eliminación de comentarios innecesarios y de métodos sin referencias

--- Models/Users.cs
+++ Models/Users.cs
@@ -5,7 +5,7 @@ using WebApi.Repositories;
 namespace WebApi.Models
 {
     [BsonCollection("Users")]
-    public class Users :Document
+    public class Users : Document
     {
 
         public string Name { get; set; } = "";
@@ -22,22 +22,14 @@ namespace WebApi.Models
         {
             public string Name { get; set; }
             public string LastName { get; set; }
-
-            //public string Gender { get; set; }
-            //public int Age { get; set; } 
-            //Creo que al devolver un usuario su genero y su edad es irrelevante
-
             public Addresses Address { get; set; }
             public bool HasDisability { get; set; }
             public decimal? DisabilityDegree { get; set; }
             string Email { get; set; }
             object Id { get; }
-        }
-    }
 
-    public class Login
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
+            //Creo que en este caso devolver un usuario con su genero y su edad es irrelevante
+
+        }
     }
 }
