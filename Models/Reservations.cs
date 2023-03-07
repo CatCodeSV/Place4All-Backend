385ecbe22e6c7e7f566d66b2f314e4ccf233a477
Laura Gil <a25589svalero.com>
2023-03-07 20:20:10 +0100

    Eliminadas referencias que ya no se utilizan

--- Models/Reservations.cs
+++ Models/Reservations.cs
@@ -2,10 +2,11 @@ using System.Collections.Generic;
 using WebApi.Repositories;
 
 namespace WebApi.Models;
+
 [BsonCollection("Reservations")]
 public class Reservations : Document
 {
-    public Users.IUser User { get; set; }
+    public Users User { get; set; }
     public Restaurants Restaurant { get; set; }
     public int Personas { get; set; }
     public List<Features> Servicios { get; set; }
