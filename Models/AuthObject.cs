ee3b31f3ea21cb4a3c117936e0fe56f40e8589eb
Laura Gil <a25589svalero.com>
2023-03-07 18:22:31 +0100

    Creado Objeto AuthObject que consta de la información de un usuario junto a su token correspondiente

---
+++ Models/AuthObject.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApi.Models
+{
+    public class AuthObject
+    {
+        public string? Token { get; set; }
+        public Users? User { get; set; }
+
+        public AuthObject()
+        {
+            Token = string.Empty;
+            User = new Users();
+        }
+
+        public AuthObject (string token, Users user)
+        {
+            Token = token;
+            User = user;
+        }
+    }
+}
