726cd532de78a4834ac5c2f576f3157e65c8ea12
Alexander Aguirre Mora <aaguirrem@hiberus.com>
2023-03-21 11:03:11 +0100

    Se quitan importaciones no usadas

--- Program.cs
+++ Program.cs
@@ -1,25 +1,12 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using WebApi.Models;
 using WebApi.Repositories;
 using WebApi.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
 using System.Text;
-using System.Runtime.CompilerServices;
-using Microsoft.Extensions.Configuration;
 using Serilog;
-using Serilog.Configuration;
-using Serilog.Core;
-using Serilog.Events;
-using Serilog.Formatting.Json;
 
 namespace WebApi
 {
