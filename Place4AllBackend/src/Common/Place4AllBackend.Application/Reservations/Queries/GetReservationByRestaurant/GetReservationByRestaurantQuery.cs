474b279ec599823cb63c0470f5212e839a1fc7ea
SANDARA\Sandra <a25660@svalero.com>
2023-04-21 21:10:52 +0200

    añado el query de GetByIdReservation

---
+++ Place4AllBackend/src/Common/Place4AllBackend.Application/Reservations/Queries/GetReservationByRestaurant/GetReservationByRestaurantQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Place4AllBackend.Application.Reservations.Queries.GetReservationByRestaurant
+{
+    internal class GetReservationByRestaurantQuery
+    {
+    }
+}
