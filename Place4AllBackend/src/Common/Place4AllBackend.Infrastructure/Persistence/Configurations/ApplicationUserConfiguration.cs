c240d895009d5d148d79eeb7c7defc252b42ec79
Alexander Aguirre <keevinaguirre@gmail.com>
2023-04-22 17:31:34 +0200

    Fix de Configuracion de user

--- Place4AllBackend/src/Common/Place4AllBackend.Infrastructure/Persistence/Configurations/ApplicationUserConfiguration.cs
+++ Place4AllBackend/src/Common/Place4AllBackend.Infrastructure/Persistence/Configurations/ApplicationUserConfiguration.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Place4AllBackend.Domain.Entities;
-using Place4AllBackend.Infrastructure.Identity;
 
 namespace Place4AllBackend.Infrastructure.Persistence.Configurations;
 
@@ -10,6 +9,8 @@ public class ApplicationUserConfiguration : IEntityTypeConfiguration<Application
     public void Configure(EntityTypeBuilder<ApplicationUser> builder)
     {
         builder.HasOne(x => x.Address).WithOne().HasForeignKey<ApplicationUser>(x => x.AddressId);
-        //builder.HasMany(x => x.FavoriteRestaurants).WithMany(x => x.FavoriteUsers) TODO: El listado de usuarios en Restaurantes no es accesible
+        builder.HasMany(x => x.FavoriteRestaurants).WithMany(x => x.FavoriteUsers)
+            .UsingEntity(j =>
+                j.ToTable("FavoriteRestaurants"));
     }
 }
