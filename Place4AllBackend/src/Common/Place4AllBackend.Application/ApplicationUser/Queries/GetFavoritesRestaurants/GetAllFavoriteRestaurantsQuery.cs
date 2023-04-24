11980a29880b15b33ab0dfbf56541ce90b67066b
Alexander Aguirre <keevinaguirre@gmail.com>
2023-04-24 12:22:32 +0200

    Fix de query

--- Place4AllBackend/src/Common/Place4AllBackend.Application/ApplicationUser/Queries/GetFavoritesRestaurants/GetAllFavoriteRestaurantsQuery.cs
+++ Place4AllBackend/src/Common/Place4AllBackend.Application/ApplicationUser/Queries/GetFavoritesRestaurants/GetAllFavoriteRestaurantsQuery.cs
@@ -11,6 +11,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Mapster;
 
 namespace Place4AllBackend.Application.ApplicationUser.Queries.GetFavoritesRestaurants
 {
@@ -18,22 +19,32 @@ namespace Place4AllBackend.Application.ApplicationUser.Queries.GetFavoritesResta
     {
     }
 
-    public class GetRestaurantsHandler : IRequestHandlerWrapper<GetAllFavoriteRestaurantsQuery, List<RestaurantDto>>
+    public class
+        GetFavoriteRestaurantsHandler : IRequestHandlerWrapper<GetAllFavoriteRestaurantsQuery, List<RestaurantDto>>
     {
         private readonly IApplicationDbContext _context;
         private readonly IMapper _mapper;
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IIdentityService _identityService;
 
-        public GetFavoriteRestaurantsHandler(IApplicationDbContext context, IMapper mapper)
+        public GetFavoriteRestaurantsHandler(IApplicationDbContext context, IMapper mapper,
+            ICurrentUserService currentUserService, IIdentityService identityService)
         {
             _context = context;
             _mapper = mapper;
+            _currentUserService = currentUserService;
+            _identityService = identityService;
         }
 
-        public async Task<ServiceResult<List<RestaurantDto>>> Handle(GetAllFavoriteRestaurantsQuery request, CancellationToken cancellationToken)
+
+        public async Task<ServiceResult<List<RestaurantDto>>> Handle(GetAllFavoriteRestaurantsQuery request,
+            CancellationToken cancellationToken)
         {
-            var list = await _context.Restaurants.Include(a => a.Name).ToListAsync(cancellationToken);
-            
-            return list.Count>0
+            var user = await _identityService.GetFavoriteRestaurants(_currentUserService.UserId);
+            var list = await _context.Restaurants.Where(x => x.FavoriteUsers.Contains(user)).Include(x => x.Address)
+                .Include(x => x.Features).Include(x => x.Images).ProjectToType<RestaurantDto>(_mapper.Config)
+                .ToListAsync(cancellationToken);
+            return list.Count > 0
                 ? ServiceResult.Success(list)
                 : ServiceResult.Failed<List<RestaurantDto>>(ServiceError.NotFound);
         }
