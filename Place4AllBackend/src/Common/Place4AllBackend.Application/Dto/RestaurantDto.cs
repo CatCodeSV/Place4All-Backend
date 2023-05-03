59af79c6911c2a471e22b63d777d93a4e0893a2e
Alexander Aguirre <keevinaguirre@gmail.com>
2023-05-03 18:51:39 +0200

    Se agrega la el listado de reviews

--- Place4AllBackend/src/Common/Place4AllBackend.Application/Dto/RestaurantDto.cs
+++ Place4AllBackend/src/Common/Place4AllBackend.Application/Dto/RestaurantDto.cs
@@ -13,8 +13,12 @@ public class RestaurantDto : IRegister
     public AddressDto Address { get; set; }
     public string Description { get; set; }
     public string PhoneNumber { get; set; }
+    public double Rating { get; set; } = 0;
+    public int NumberOfReviews { get; set; } = 0;
     public List<ImageDto> Images { get; set; }
     public List<FeatureDto> Features { get; set; }
+    public List<ReviewDto> Reviews { get; set; }
+    
     public string CreateDate { get; set; }
 
     public void Register(TypeAdapterConfig config)
--- Place4AllBackend/src/Common/Place4AllBackend.Application/Restaurants/Queries/GetRestaurantById/GetRestaurantByIdQuery.cs
+++ Place4AllBackend/src/Common/Place4AllBackend.Application/Restaurants/Queries/GetRestaurantById/GetRestaurantByIdQuery.cs
@@ -21,18 +21,26 @@ namespace Place4AllBackend.Application.Restaurants.Queries.GetRestaurantById
         private readonly IApplicationDbContext _context;
         private readonly IMapper _mapper;
 
-        public GetRestaurantByIdQueryHandler (IApplicationDbContext context, IMapper mapper)
+        public GetRestaurantByIdQueryHandler(IApplicationDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
 
-        async Task<ServiceResult<RestaurantDto>> IRequestHandler<GetRestaurantByIdQuery, ServiceResult<RestaurantDto>>.Handle(GetRestaurantByIdQuery request, CancellationToken cancellationToken)
+        async Task<ServiceResult<RestaurantDto>> IRequestHandler<GetRestaurantByIdQuery, ServiceResult<RestaurantDto>>.
+            Handle(GetRestaurantByIdQuery request, CancellationToken cancellationToken)
         {
-            var restaurant = await _context.Restaurants.Where(x => x.Id == request.RestaurantId).Include(a => a.Address).Include(f => f.Features).Include(i => i.Images).ProjectToType<RestaurantDto>(_mapper.Config).FirstOrDefaultAsync(cancellationToken);
+            var restaurant = await _context.Restaurants.Where(x => x.Id == request.RestaurantId).Include(a => a.Address)
+                .Include(f => f.Features).Include(r => r.Reviews).Include(i => i.Images)
+                .Select(r => new RestaurantDto()
+                {
+                    NumberOfReviews = r.Reviews.Count
+                }).ProjectToType<RestaurantDto>(_mapper.Config)
+                .FirstOrDefaultAsync(cancellationToken);
 
-            return restaurant != null ? ServiceResult.Success(restaurant) : ServiceResult.Failed<RestaurantDto>(ServiceError.NotFound);
+            return restaurant != null
+                ? ServiceResult.Success(restaurant)
+                : ServiceResult.Failed<RestaurantDto>(ServiceError.NotFound);
         }
     }
-
 }
