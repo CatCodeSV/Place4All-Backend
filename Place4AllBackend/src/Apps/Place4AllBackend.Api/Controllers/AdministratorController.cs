debef6fce8d9f9bcc18083416f9fc7813580983a
Laura Gil <a25589svalero.com>
2023-06-06 19:34:15 +0200

    Creado controlador Administrador

---
+++ Place4AllBackend/src/Apps/Place4AllBackend.Api/Controllers/AdministratorController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Place4AllBackend.Api.Controllers
+{
+    [Authorize (Roles = "Administrator")]
+    public class AdministratorController : BaseApiController
+    {
+    }
+}
