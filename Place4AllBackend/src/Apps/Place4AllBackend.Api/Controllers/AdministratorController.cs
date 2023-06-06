bbd1a3becb78eba05b5c7c9d601b032bcb840de9
Laura Gil <a25589svalero.com>
2023-06-06 22:12:43 +0200

    Añadidos getters restaurants

--- Place4AllBackend/src/Apps/Place4AllBackend.Api/Controllers/AdministratorController.cs
+++ Place4AllBackend/src/Apps/Place4AllBackend.Api/Controllers/AdministratorController.cs
@@ -1,9 +1,70 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Place4AllBackend.Application.Common.Models;
+using Place4AllBackend.Application.Dto;
+using Place4AllBackend.Application.Restaurants.Queries.GetRestaurants;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using Place4AllBackend.Application.Restaurants.Queries.GetRestaurantsByFeatures;
+using Place4AllBackend.Application.Restaurants.Queries.GetRestaurantById;
+using Place4AllBackend.Application.Restaurants.Queries.GetFavoritesRestaurants;
 
 namespace Place4AllBackend.Api.Controllers
 {
     [Authorize (Roles = "Administrator")]
     public class AdministratorController : BaseApiController
     {
+        /// <summary>
+        /// Get all Restaurants
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<ServiceResult<List<RestaurantDto>>>> GetAllRestaurants_admin(
+            CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(new GetAllRestaurantsQuery(), cancellationToken));
+        }
+
+        /// <summary>
+        /// Get restaurant by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ServiceResult<RestaurantDto>>> GetRestaurantById_admin(int id,
+            CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(new GetRestaurantByIdQuery { RestaurantId = id }, cancellationToken));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpPost("features")]
+        public async Task<ActionResult<ServiceResult<List<RestaurantDto>>>> GetRestaurantsByFeature_admin(
+            GetRestaurantsByFeatureQuery query, CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(new GetRestaurantsByFeatureQuery(), cancellationToken));
+        }
+
+        /// <summary>
+        /// Get Favorite Restaurants
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpGet("User")]
+        public async Task<ActionResult<ServiceResult<List<RestaurantDto>>>> GetUserFavoriteRestaurants_admin(
+            CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(new GetFavoriteRestaurantsQuery(), cancellationToken));
+        }
+
+
     }
 }
